f33462742d97fdc655733940a764ad290d32f689
Giacomo Nanni <gn.nanni@gmail.com>
2023-02-27 11:59:20 +0100

    fix: allow insecure request in dev

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,15 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header("HTTP/1.1 200 OK");
+}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: content-type, authorization, origin, x-requested-with');
+header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
 return [
   'api' => [
     'basicAuth' => true,
+    'allowInsecure' => true // Use in development
   ]
 ];
