bb532c3b1c73eea6f519dac570d730d5cfdfe965
Giacomo Nanni <gn.nanni@gmail.com>
2024-12-08 11:20:59 +0100

    backend setup

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,5 @@
-<?= $site->title() ?>
+<strong><?= $site->title() ?></strong><br><br>
+
+Admin Login: <a href="<?= $site->url() ?>/panel">Link</a>
+<br>
+Api: <a href="<?= $site->url() ?>/api/query">Link</a><br>
